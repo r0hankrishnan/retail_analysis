74a4351617f710529e80fd4bfc48c0c9d9467e62
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-10-29 00:37:01 -0400

    reformat

--- notebooks/scripts/useful_functions.py
+++ notebooks/scripts/useful_functions.py
@@ -1,15 +1,28 @@
 def load_data():
     import pandas as pd
-    df = pd.read_excel('../data/online_retail.xlsx')
+    df = pd.read_excel('../data/raw/online_retail.xlsx')
     return df
 
 
 def load_clean_data():
     import pandas as pd
     try:
-        df = pd.read_csv('../data/clean_retail.csv', index_col=0)
+        df = pd.read_csv('../data/cleaned/clean_retail.csv', index_col=0)
     except Exception as e:
         print(f"Error: {e}")
         
     print("Data loaded successfully.")
+    return df
+
+def load_analysis_data(relPath:str):
+    import pandas as pd
+    
+    try:
+        df = pd.read_csv(f"../data/analysis{relPath}", index_col=0)
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    if "Unnamed: 0" in df.columns:
+        df = df.drop("Unnamed: 0", axis = 1)
+    
     return df
