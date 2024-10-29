965dd652b1a5bf1c35a0649be2d5d14864395c93
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-10-28 20:36:39 -0400

    Update useful_functions.py

--- notebooks/scripts/useful_functions.py
+++ notebooks/scripts/useful_functions.py
@@ -2,3 +2,14 @@ def load_data():
     import pandas as pd
     df = pd.read_excel('../data/online_retail.xlsx')
     return df
+
+
+def load_clean_data():
+    import pandas as pd
+    try:
+        df = pd.read_csv('../data/clean_retail.csv', index_col=0)
+    except Exception as e:
+        print(f"Error: {e}")
+        
+    print("Data loaded successfully.")
+    return df
