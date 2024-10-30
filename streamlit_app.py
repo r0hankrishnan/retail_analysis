ee62bb6a9eb24ad87b61db0047c6f13d96c72bbc
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-10-29 20:39:23 -0400

    streamlit dashboard additions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,22 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import datetime as dt
+import plotly.express as px
+from sklearn.cluster import KMeans
 
+#########################
+####### CONFIGS
+#########################
 st.set_page_config(
     page_title="Clustering Analysis",
     page_icon=":material/communities:",
     layout="wide"
 )
 
+#########################
+####### CACHE
+#########################
 @st.cache_data(max_entries=2)
 def load_clean_data():
     df = pd.read_csv('./data/cleaned/clean_retail.csv')
@@ -18,13 +27,36 @@ def load_clean_data():
     df["Revenue"] = df["UnitPrice"] * df["Quantity"]
     return df
 
-st.title("Customer Clustering Dashboard")
+@st.cache_resource(max_entries=1)
+def load_KMeans():
+    kmeans = KMeans()
+    return kmeans
+
+#########################
+####### GLOBAL OBJECTS
+#########################
 
 data = load_clean_data()
 
 
+#########################
+####### UI
+#########################
 
-agg_data = data.groupby("CustomerID", as_index=False)\
+st.title("Customer Clustering Dashboard")
+
+with st.expander(label="Filter Data",
+                 icon=":material/filter_list:"):
+    country_filter = st.multiselect(label="Filter by country:",
+                                options=data["Country"].unique(), 
+                                default=None)
+
+if country_filter:
+    show_data = data[data["Country"].isin(country_filter)]
+else:
+    show_data = data
+    
+agg_data = show_data.groupby("CustomerID", as_index=False)\
     .agg(
         LTDValue=("Revenue", "sum"),
         PurchaseFrequency = ("InvoiceNo", "nunique"),
@@ -34,11 +66,38 @@ agg_data = data.groupby("CustomerID", as_index=False)\
 max_date = pd.to_datetime(data["InvoiceDate"]).max()
 
 agg_data["PurchaseRecency"] = (max_date - pd.to_datetime(agg_data["MostRecentPurchase"])).dt.days
+agg_data["CustomerID"] = agg_data["CustomerID"].astype(int).astype(str)
+
+top_item = show_data["Description"].mode()
+if len(top_item) != 1:
+    top_item = "Multiple most popular items"
+else:
+    top_item = top_item.item()
+
+avg_rev = round(show_data["Revenue"].mean(),2)
 
 
-st.subheader("Cleaned Data")
-st.dataframe(data)
+c1, c2 = st.columns(2)
 
-st.subheader("Grouped Data")
-st.dataframe(agg_data[["CustomerID", "LTDValue", "PurchaseFrequency", "PurchaseRecency"]])
+with c1.container(border=True):
+    st.metric(label="Most Popular Item",
+            value=top_item.title(),
+            delta=+1)
+    
+with c2.container(border=True):
+    st.metric(label="Average Revenue",
+              value=f"${avg_rev}",
+              delta=+1)
 
+
+c1, c2 = st.columns([0.55,0.45])
+with c1:
+    st.subheader("Cleaned Data")
+    st.dataframe(show_data,
+                 hide_index=True,
+                 use_container_width=True)
+with c2:
+    st.subheader("Grouped Data")
+    st.dataframe(agg_data[["CustomerID", "LTDValue", "PurchaseFrequency", "PurchaseRecency"]],
+                 hide_index=True,
+                 use_container_width=True)
